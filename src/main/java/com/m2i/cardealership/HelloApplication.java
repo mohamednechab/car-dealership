0729319f43fe2417cd6ff0a098cd184d295871d5
ubuntu <mohamednechab.mn@gmail.com>
2022-04-12 15:11:40 +0200

    send project

---
+++ src/main/java/com/m2i/cardealership/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.m2i.cardealership;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
